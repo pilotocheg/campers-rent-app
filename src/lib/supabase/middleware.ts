18d4393d8bce979c11aafa69b59d0ed460156858
Viacheslav Dmytriiev <v.dmytriiev@sdventures.com>
2026-01-30 20:31:40 +0100

    Update env names

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -12,8 +12,8 @@ export async function updateSession(request: NextRequest) {
   });
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_KEY!,
     {
       cookies: {
         getAll() {
